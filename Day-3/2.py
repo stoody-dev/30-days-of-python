d38c055efcb2ec4fc583738af5972877eb65d6b8
AKSHAY <forakshay.dev@gmail.com>
2023-02-06 22:15:46 +0530

    /

--- Day-3/2.py
+++ Day-3/2.py
@@ -8,4 +8,4 @@ print("My Name is",b)
 #arithmatic integers between two integers 
 x=input("enter first number:")
 y=input("enter second solution:")
-print(int(x)+int(y))
+print(int(x)+int(y))#typecasting required 
---
+++ Day-4/1.py
@@ -0,0 +1,20 @@
+#strings in python
+#in python you enclose anything between single and double quotes is called as string
+name ="akshay"
+game ="valorant"
+print ("i want to play ", game) #for multi line string we use triple single quotes
+
+print (name[0])#indexing in string starts from 0 to size-1 in python
+print (name[1])
+print (name[2])
+print (name[3])
+print (name[4])
+print (name[5])#on using print(name[6] we get an error because there is nothing at 6th index)
+
+intro='''
+hi 
+i am stoody-dev
+a weeb dev xd
+'''
+for character in intro:
+    print (character)
