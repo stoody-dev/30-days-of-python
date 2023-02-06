30644361283615f3904dec00e884294fbff57f26
AKSHAY <forakshay.dev@gmail.com>
2023-02-06 21:59:10 +0530

    Update 2.py

--- Day-3/2.py
+++ Day-3/2.py
@@ -4,3 +4,8 @@ print(a)
 
 b=input("Enter your name: ")
 print("My Name is",b)
+
+#arithmatic integers between two integers 
+x=input("enter first number:")
+y=input("enter second solution:")
+print(int(x)+int(y))
